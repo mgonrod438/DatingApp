d664519f40e48c659175a3c610ac2e84f3470891
mgonrod438 <marta.gonzalez@getronics.com>
2023-03-28 01:47:21 +0200

    End of section 10

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
@@ -8,17 +9,26 @@ import { Member } from '../_models/member';
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members:Member[] = [];
 
   constructor(private http: HttpClient) { }
 
   getMember(username: string){
+    const member = this.members.find(x=>x.userName === username);
+    if (member) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOption())
+    if (this.members.length>0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl + 'users'/** , this.getHttpOption()*/).pipe(
+      map(members => {
+        this.members= members;
+        return members;
+      })
+    )
   }
 
-  getHttpOption(){
+  /** getHttpOption(){
     const userString = localStorage.getItem('user');
     if (!userString) return;
     const user= JSON.parse(userString);
@@ -28,5 +38,14 @@ export class MembersService {
       })
       
     }
+  }*/
+
+  updateMember(member: Member){
+    return this.http.put(this.baseUrl+'users',member).pipe(
+      map(()=>{
+        const index = this.members.indexOf(member);
+        this.members[index] = {...this.members[index], ...member}
+      })
+    )
   }
 }
