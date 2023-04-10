84ed70eae3fe96d2f651f8c804dfdb168c84036d
mgonrod438 <marta.gonzalez@getronics.com>
2023-04-10 18:09:07 +0200

    middle of section 19

--- API/Program.cs
+++ API/Program.cs
@@ -27,9 +27,13 @@ app.UseCors(builder => builder
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
+app.MapFallbackToController("Index", "Fallback");
 
 
 using var scope = app.Services.CreateScope();
